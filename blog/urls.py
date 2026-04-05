e2f9cb21c4ae86de347f3cca4e54af6377604806
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-04-05 22:41:53 +0200

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.starting_page, name='starting-page'),
+    path('posts/', views.posts, name='posts-page'),
+    path('posts/<slug:slug>/', views.post_detail, name='post-detail-page')
+]
