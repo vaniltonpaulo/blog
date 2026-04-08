7e9fe575bf601c7c7d5f63c2297075aafad71f96
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-04-08 11:40:18 +0200

    Registering Models for Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Post)           
+admin.site.register(Tag)
+
+
+
