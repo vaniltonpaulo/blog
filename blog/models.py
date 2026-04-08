7d80ff422158ba3cbd0759fe62330b98f719b89a
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-04-08 11:25:29 +0200

    Adding a Post Mode

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, validators=[MinLengthValidator(10)])
+    content = models.TextField()
+    # image = models.CharField(max_length=100)
+    # author = models.CharField(max_length=50)
+
+
