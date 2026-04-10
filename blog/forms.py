e66a18d91edde88f2a7427d1d45a128702d9c85f
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-04-10 10:24:58 +0200

    Styling the Comment Form

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+    comment = forms.CharField(widget=forms.Textarea, label='Your Comment')
     class Meta:
         model = Comment
         exclude = ['post']  # We will set the post field in the view, so we exclude it from the form
